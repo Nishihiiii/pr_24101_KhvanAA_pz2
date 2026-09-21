85f5f3fdd072789dc937f19701563441ddd866b5
Anastasia <anastasiahvan972@gmail.com>
2026-09-21 22:20:52 +0700

    pz no.2

---
+++ app/src/main/java/com/example/pr_24101_khvanaa_pz2/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.pr_24101_khvanaa_pz2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pr_24101_khvanaa_pz2.ui.theme.Pr_24101_KhvanAA_pz2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pr_24101_KhvanAA_pz2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Pr_24101_KhvanAA_pz2Theme {
+        Greeting("Android")
+    }
+}
